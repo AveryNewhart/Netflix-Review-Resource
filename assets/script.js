69c1e5932722c127e305878a55f0908b3d9e219a
AveryNewhart <newhartreeceavery@gmail.com>
2022-12-20 20:49:43 -0500

    new files

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+//Referencing ids/classes in the html file
+let searchButtonEl = document.getElementById("clickSearch");
+let listSearchHistEl = document.getElementById("listSearchHistory");
+let userSearchEl = document.getElementById("searchedMovie");
+let userSearchElValue = document.getElementById("searchedMovie").value;
+
+//searching movie to get rotten tomatoe/other review
+searchButtonEl.addEventListener('click', function() {
+    let userSearchElValue = document.getElementById("searchedMovie").value;
+})
+
+//MIGHT COMBINE FUNCTIONS EVEN? JUST WANTED A BASIS TO HAVE A LINEOUT OF WHAT NEEDS TO BE DONE
+
+//searching movie to get netflix description
+searchButtonEl.addEventListener('click', function() {
+    let userSearchElValue = document.getElementById("searchedMovie").value
+})
